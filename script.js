c4ed987f07536a3d54dd710a846ac0ca247289ca
Francisco González <franciscogonza2002@gmail.com>
2021-07-10 23:07:42 +0000

    Modified with Theia

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function focus() {
-  document.getElementById("principal").focus();
+    document.getElementById("principal").focus();
 }
-
+  
 function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100; //Interest calculation
     var year = new Date().getFullYear()+parseInt(years); //Adds the actual year to the selected number of years
-    
-    if (principal <= 0) { //Principal Validation
+      
+    //Adding Text to the Span class with "result" ID
+    if (principal <= 0 || principal == null) { //Principal Validation
         alert("Enter a positive number greater than 0.");
         focus(); //Focus on the "principal" field
     }
     else {
-        //Adding Text to the Span class with "result" ID
         document.getElementById("result").innerHTML = "If you deposit \<mark\>"+principal+"\</mark\>,\<br\> at an interest rate of \<mark\>"+rate+"%\</mark\>\<br\> You will receive an amount of \<mark\>"+interest+"\</mark\>,\<br\> in the year \<mark\>"+year+"\</mark\>\<br\>"
     }
 }
-
+  
 function updateRate() { //Changing the value of "rate_val" span when range changes
     var rateVal = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateVal + "%";
